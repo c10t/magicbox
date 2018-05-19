49b840b5a1df1c50d3822fd16f9b1cf16b938385
c10t <c10t@fa.ke>
2018-05-19 22:59:42 +0900

    initial commit

---
+++ src/main/java/com/potteryd/magicbox/MagicboxApplication.java
@@ -0,0 +1,12 @@
+package com.potteryd.magicbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicboxApplication.class, args);
+	}
+}
