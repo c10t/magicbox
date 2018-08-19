b8eb8ee2904799ec94412252e1e030c7dd14ae48
c10t <c10t@fa.ke>
2018-08-20 07:25:49 +0900

    initialize

---
+++ src/main/java/com/c10t/magicbox/MagicboxApplication.java
@@ -0,0 +1,12 @@
+package com.c10t.magicbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicboxApplication.class, args);
+	}
+}
